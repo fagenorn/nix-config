5b05fd5132d40c4fea7d0fb484d16d2c12494e94
Anis Sakkaf <anisanissakkaf@gmail.com>
2025-04-26 03:02:10 +0200

    feat: update aerospace configuration for Ghostty and add default settings for Ghostty

--- home/ghostty/default.nix
+++ home/ghostty/default.nix
@@ -1,12 +1,19 @@
 {...}: {
   programs.ghostty = {
     settings = {
-      # MacOS settings
+      theme = "dark:catppuccin-frappe";
       window-title-font-family = "Fira Code Medium";
       macos-option-as-alt = true;
       macos-window-shadow = false;
       macos-auto-secure-input = true;
       macos-secure-input-indication = true;
+      macos-titlebar-style = tabs;
+      macos-non-native-fullscreen = true;
+
+      keybind = "global:cmd+shift+f=toggle_fullscreen";
+      keybind = "global:f11=toggle_quick_terminal";
+
+      background-opacity = 0.93
     };
   };
 }
