70ddf169d505f1b3473ab613377af77dfbdbe875
Anis Sakkaf <anisanissakkaf@gmail.com>
2025-04-26 23:15:49 +0200

    sops#all

--- home/sops/default.nix
+++ home/sops/default.nix
@@ -1,31 +1,22 @@
-{ myvars, config, ... }:
+{
+  lib,
+  myvars,
+  config,
+  ...
+}:
 {
   sops = {
-    age.keyFile = myvars.sops.age.keyFile;
+    age.sshKeyPaths = [ "${config.home.homeDirectory}/.ssh/id_ed25519" ];
+    age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
     defaultSopsFile = myvars.sops.defaultSopsFile;
 
     secrets = {
-      my_ssh_private_key.path = "${config.home.homeDirectory}/.ssh/id_ed25519";
-      my_ssh_public_key.path = "${config.home.homeDirectory}/.ssh/id_ed25519.pub";
+      "my_ssh_private_key" = {
+        path = "${config.home.homeDirectory}/.ssh/id_ed25519";
+      };
+      "my_ssh_public_key" = {
+        path = "${config.home.homeDirectory}/.ssh/id_ed25519.pub";
+      };
     };
-
-    # templates = {
-    #   "my_ssh_private_key" = {
-    #     content = config.sops.placeholder.my_ssh_private_key;
-    #   };
-    #   "my_ssh_public_key" = {
-    #     content = config.sops.placeholder.my_ssh_public_key;
-    #   };
-    # };
   };
-
-  # home.file.".ssh/id_ed25519" = {
-  #   # enable = true;
-  #   source = config.sops.secrets.my_ssh_private_key.path;
-  # };
-
-  # home.file.".ssh/id_ed25519.pub" = {
-  #   enable = true;
-  #   source = config.sops.templates.my_ssh_public_key.path;
-  # };
 }
