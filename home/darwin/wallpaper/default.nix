354afea7fb113e92c7c3226a961e6a6b7b4b9392
Anis Sakkaf <anisanissakkaf@gmail.com>
2025-04-28 19:47:18 +0200

    darwin wallpaper #2

--- home/darwin/wallpaper/default.nix
+++ home/darwin/wallpaper/default.nix
@@ -1,23 +1,9 @@
 {
+  config,
   lib,
+  pkgs,
   ...
 }:
-with lib;
-let
-  cfg = lib.mkDefault ../../../assets/woah.jpg;
-in
 {
-  options = {
-    services.darwin-wallpaper.file = mkOption {
-      type = with types; nullOr path;
-      default = null;
-      description = "Path to image for wallpaper";
-    };
-  };
-
-  home.activation = {
-    setDarwinWallpaper = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      osascript -e 'tell application "Finder" to set desktop picture to "${cfg.file}" as POSIX file'
-    '';
-  };
+  services.darwin-wallpaper.file = lib.mkDefault ./hearts.png;
 }
