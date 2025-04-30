596cf74af2f042a5e76eee576814521b99a10e64
Anis Sakkaf <anisanissakkaf@gmail.com>
2025-04-30 13:41:03 +0200

    remove Fantastical app entry from dock configuration

--- hosts/common/darwin-common-dock.nix
+++ hosts/common/darwin-common-dock.nix
@@ -6,7 +6,6 @@
       "/Applications/WhatsApp.app"
       "/Applications/Discord.app"
       "/Applications/Slack.app"
-      "/Applications/Fantastical.app"
       "/Applications/Visual Studio Code.app"
       "/Applications/Spotify.app"
       "/Applications/Ghostty.app"
